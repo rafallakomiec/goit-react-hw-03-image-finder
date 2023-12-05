af8288df7a41c3918fc7a10c5822ab01ca73a97b
Rafał Łakomiec <rafal.lakomiec@gmail.com>
2023-12-05 18:20:31 +0100

    Create Searchbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { Component } from 'react';
+import { Searchbar } from './components/Searchbar/Searchbar';
 
 class App extends Component {
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={ }/>    
+      </>
+    );
+  }
   
 }
 
