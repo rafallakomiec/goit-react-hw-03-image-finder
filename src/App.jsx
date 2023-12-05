3fddf1862e541253e0f219c349ebdae8ef94187d
Rafał Łakomiec <rafal.lakomiec@gmail.com>
2023-12-05 19:45:41 +0100

    Create basic image load

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { Searchbar } from './components/Searchbar/Searchbar';
 import { fetchHandler } from './utils/fetchHandlers/fetchHandler';
+import { ImageGallery } from './components/ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './components/ImageGalleryItem/ImageGalleryItem';
 
 class App extends Component {
 
@@ -14,14 +16,26 @@ class App extends Component {
     const searchPhrase = event.target.elements.searchInput.value;
     this.setState({pageNo: 1}, () => { 
       const response = fetchHandler(searchPhrase, this.state.pageNo).hits;
+      this.setState({ images: response });
     });
     
   }
 
   render() {
+    const imageGalleryItems = this.state.images.map(item => {
+      return (
+        <ImageGalleryItem key={item.id} description={item.tags} image={item.webFormatURL} />
+      );
+    });
+
     return (
       <>
-        <Searchbar onSubmit={this.onSubmitHandler}/>    
+        <Searchbar onSubmit={this.onSubmitHandler} />
+        { this.state.images.length > 0 &&
+          <ImageGallery>
+            {imageGalleryItems}
+          </ImageGallery>
+        }
       </>
     );
   }
